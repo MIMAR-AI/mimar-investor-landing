6d95ded33d61e66c07bce208637eb0dc2afd8f64
ahmadabdulmajeed <ahmad.abdulmajeed@foundifai.com>
2026-08-28 10:33:40 +0300

    Serve investor model assets

--- server.js
+++ server.js
@@ -10,10 +10,18 @@ const files = new Map([
   ['/styles.css', 'styles.css'],
   ['/brand.css', 'brand.css'],
   ['/motion.css', 'motion.css'],
+  ['/data.css', 'data.css'],
   ['/script.js', 'script.js'],
-  ['/assets/mimar-mark.svg', 'assets/mimar-mark.svg']
+  ['/assets/mimar-mark.svg', 'assets/mimar-mark.svg'],
+  ['/assets/MIMAR_Investor_Revenue_Model_2026-08-28.xlsx', 'assets/MIMAR_Investor_Revenue_Model_2026-08-28.xlsx']
 ]);
-const types = { '.html': 'text/html; charset=utf-8', '.css': 'text/css; charset=utf-8', '.js': 'text/javascript; charset=utf-8', '.svg': 'image/svg+xml' };
+const types = {
+  '.html': 'text/html; charset=utf-8',
+  '.css': 'text/css; charset=utf-8',
+  '.js': 'text/javascript; charset=utf-8',
+  '.svg': 'image/svg+xml',
+  '.xlsx': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+};
 
 createServer(async (req, res) => {
   const path = new URL(req.url, `http://${req.headers.host}`).pathname;
